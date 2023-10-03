ac9a6ce99a21efddfa8ad3e5f4edc6500079d4c4
basilaslam <basilaslamnp@gmail.com>
2023-10-03 14:23:30 +0530

    stripe checkout succeeded -have to implement settings page

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,72 +1,76 @@
 import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
 import prismadb from "@/lib/prisma.db";
 import { stripe } from "@/lib/stripe";
-import { absoluteURL } from "@/lib/utils";
-import { metadata } from "@/app/layout";
+import { absoluteUrl } from "@/lib/utils";
 
+const settingsUrl = absoluteUrl("/settings");
 
-const settingsUrl = absoluteURL('/settings')
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
 
-export const GET = async () => {
-    try{
-        const { userId } = auth()
-        const user = await currentUser()
+    if (!userId || !user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 
-        if(!user || !userId){
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-
-        const userSubscription = await prismadb.userSubscription.findUnique({
-            where: {
-                userId
-            }
-        })
-
-        if(userSubscription && userSubscription.stripeCustomerId){
-            const stripeSession = await stripe.billingPortal.sessions.create({
-                customer: userSubscription.stripeCustomerId,
-                return_url: settingsUrl,
-            });
-
-
-            return new NextResponse(JSON.stringify({url: stripeSession.url}))
-
-        }
-
-    const stripeSession = await stripe.checkout.sessions.create({
-        success_url: settingsUrl,
-        cancel_url: settingsUrl,
-        payment_method_types: ["card"],
-        mode: "subscription",
-        billing_address_collection: "auto",
-        customer_email: user.emailAddresses[0].emailAddress,
-        line_items: [
-            {
-                price_data: {
-                    currency: "USD",
-                    product_data: {
-                        name: "Genius Pro",
-                        description: "Unlimited AI Generations"
-                    },
-                    unit_amount: 2000,
-                    recurring: {
-                        interval: "month"
-                    }
-                },
-                quantity: 1,
-            },
-        ],
-        metadata: {
-            userId,
-        }
-   
+    console.log(settingsUrl);
+    
+    const userSubscription = await prismadb.userSubscription.findUnique({
+      where: {
+        userId
+      }
     })
 
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsUrl,
+      })
 
-    return new NextResponse(JSON.stringify({url: stripeSession.url}))
+      console.log('chackpoint -1 ');
+      
 
-    }catch (err){
-        console.log('stripe error');
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }))
     }
-}
+
+    console.log('chackpoint -2 ');
+
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: "INR",
+            product_data: {
+              name: "Genius Pro",
+              description: "Unlimited AI Generations"
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: "month"
+            }
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    })
+    console.log('chackpoint -3 ');
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }))
+  } catch (error) {
+    console.log("[STRIPE_ERROR] line 64 - route/stripe", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
